bebd75cc1a8cedda6225fd8a92654095bb6436d6
theresanguyen <nguyentheresa204@gmail.com>
2026-01-20 18:05:14 -0800

    added empty dashbaord screen page

--- beach_circle_flutter/lib/main.dart
+++ beach_circle_flutter/lib/main.dart
@@ -4,26 +4,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'auth_screen.dart'; 
 import 'signup_screen.dart';
+import 'dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb) {
     await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyDHtKLDBgYN3ocI8zPOf8jPtE9cxHe0cnE", 
-        authDomain: "test-beach-circle.firebaseapp.com",
-        projectId: "test-beach-circle",
-        storageBucket: "test-beach-circle.firebasestorage.app",
-        messagingSenderId: "680938438818",
-        appId: "1:680938438818:web:8bd12ddd9688890dcd17fb",
-      ),
+      //insert stuff here
     );
   } else {
     // ANDROID/iOS: Use the file (google-services.json) automatically
     await Firebase.initializeApp();
   }
-  // 🔥 FIX ENDS HERE 🔥
 
   runApp(const MyApp());
 }
@@ -38,17 +31,8 @@ class MyApp extends StatelessWidget {
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          //If user is logged in, show a placeholder Home Screen
           if (snapshot.hasData) {
-            return Scaffold(
-              appBar: AppBar(title: const Text('Home')),
-              body: Center(
-                child: ElevatedButton(
-                  onPressed: () => FirebaseAuth.instance.signOut(),
-                  child: const Text('Log Out'),
-                ),
-              ),
-            );
+             return const DashboardScreen();
           }
           //Otherwise, show Auth Screen
           return const AuthScreen();
