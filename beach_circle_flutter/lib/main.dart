cb60ca992ce36d0a4902ebc3f18364d0cfd7d1f6
josuealfar38475 <josuealfaro8441@gmail.com>
2026-01-27 17:21:07 -0800

    Removing useless comments

--- beach_circle_flutter/lib/main.dart
+++ beach_circle_flutter/lib/main.dart
@@ -54,7 +54,6 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-//<<<<<<< HEAD
 }
 
 
@@ -107,5 +106,3 @@ class MyApp extends StatelessWidget {
 //     );
 //   }
 // }
-//=======
-//>>>>>>> 98e4db3fc56fc17b8a64d9cfff48495c4dbb707b
