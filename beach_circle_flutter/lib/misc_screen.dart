633f48a57e942a7408f1142ebff2a48b2442051e
tifftknguyen <tifftknguyen@gmail.com>
2026-02-16 17:24:32 -0800

    Update misc_screen.dart

--- beach_circle_flutter/lib/misc_screen.dart
+++ beach_circle_flutter/lib/misc_screen.dart
@@ -305,7 +305,7 @@ class _HeaderPill extends StatelessWidget {
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontWeight: FontWeight.w600,
-                fontSize: 12.5,
+                fontSize: 18,
                 color: Colors.black54,
               ),
             ),
