9314013ce8d4b3d5e78c2f37899138083d98b44e
tifftknguyen <tifftknguyen@gmail.com>
2026-04-01 21:59:37 -0700

    EB troubleshoot

--- beach_circle_flutter/lib/main.dart
+++ beach_circle_flutter/lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
 import 'package:flutter/foundation.dart';
-import 'auth_screen.dart'; 
+import 'firebase_options.dart';
+import 'auth_screen.dart';
 import 'signup_screen.dart';
 import 'dashboard_screen.dart';
 import 'eventboard_screen.dart';
 
-// main.dart
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb) {
-    await Firebase.initializeApp(
-      //insert stuff here
-    );
+    await Firebase.initializeApp(options: firebaseConfigWeb);
   } else {
-    // ANDROID/iOS: Use the file (google-services.json) automatically
     await Firebase.initializeApp();
   }
 
@@ -31,13 +27,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Beach Circle',
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-             return const DashboardScreen();
+            return const DashboardScreen();
           }
-          //Otherwise, show Auth Screen
           return const AuthScreen();
         },
       ),
