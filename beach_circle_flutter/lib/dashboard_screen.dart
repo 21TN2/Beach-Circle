3e89386dbc369d119c2d036e65401f89dea71335
gissyhernandez <gisellehernandez907@gmail.com>
2026-04-01 22:56:21 -0700

    These auto generated files smh + added Josues email

--- beach_circle_flutter/lib/dashboard_screen.dart
+++ beach_circle_flutter/lib/dashboard_screen.dart
@@ -320,6 +320,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
       'teef@gmail.com',
       'reytest@gmail.com',
       'giselle1@gmail.com',
+      'josuealfaro8441@gmail.com',
     ];
 
     if (_currentIndex == 0 && _homePage == "home") {
