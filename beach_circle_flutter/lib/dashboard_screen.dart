2007e7b6c10f70cb6cbf293de7432f0610efaffa
theresanguyen <nguyentheresa204@gmail.com>
2026-05-07 23:33:32 -0700

    dup line fix

--- beach_circle_flutter/lib/dashboard_screen.dart
+++ beach_circle_flutter/lib/dashboard_screen.dart
@@ -55,9 +55,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
   final PageController _pageController = PageController();
   int _currentPage = 0;
 
-  final PageController _pageController = PageController();
-  int _currentPage = 0;
-
   void _logOut() async {
     await FirebaseAuth.instance.signOut();
   }
